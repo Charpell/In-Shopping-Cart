f6c42831910216c05b5164c9ba6461254fd342cc
Ebuka Umeh <charpellumeh@gmail.com>
2022-10-16 19:37:15 +0100

    shopping cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useReducer } from 'react';
+import { cartReducer, initialState } from './reducers/cartReducer';
+import Products from './components/Products';
+import Cart from './components/Cart';
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  const fetchProducts = async () => {
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: 'ADD_PRODUCTS', payload: data.products }));
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  },[]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex' }}>
+      <Products state={state} dispatch={dispatch} />
+      <Cart state={state} dispatch={dispatch} />
     </div>
   );
 }
